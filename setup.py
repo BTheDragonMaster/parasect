ad55dde035f4378a9ffe302e957311af4d2cb8dd
Terlouw, Barbara <barbara.terlouw@wur.nl>
2024-01-10 17:28:08 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,15 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    package_data={"": ["AMP-binding_full.hmm*",
+    package_data={"": ["*.hmm*",
                        'Apositions*',
                        "*.fasta",
                        "*.txt",
                        "*.tsv",
-                       "*.classifier"]},
+                       "*class_sequence_hmm.paras",
+                       "*class_sequence_hmm_all.paras",
+                       "*class_sequence_hmm.parasect",
+                       "*class_sequence_hmm_onehot.paras",
+                       "*class_sequence_hmm_onehot.parasect"
+                       ]},
     scripts=['bin/parasect', 'bin/paras'])
