d98af24b36f52bae64f5a966ecbc7a8d367529df
David Meijer <david-meijer@live.nl>
2024-04-15 15:26:18 +0200

    FIX: skip .gitkeep when clearing temp folder

--- paras/scripts/data_processing/temp.py
+++ paras/scripts/data_processing/temp.py
@@ -4,7 +4,9 @@ import shutil
 def clear_temp(temp_dir):
 
     for temp_name in os.listdir(temp_dir):
-        if not temp_name == '__init__.py':
+        
+        # skip .gitkeep 
+        if not temp_name == '__init__.py' and not temp_name == '.gitkeep':
             file_location = os.path.join(temp_dir, temp_name)
             if os.path.isfile(file_location):
                 os.remove(file_location)
