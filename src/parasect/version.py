5c0bd1306cdcbf79581814db5b7a6246c78f3b03
David Meijer <david-meijer@live.nl>
2024-10-08 20:08:50 +0200

    REF: updated version module

--- src/parasect/version.py
+++ src/parasect/version.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Version module for PARASECT."""
+
 VERSION = "1.0.0"
+
+
+def get_version() -> str:
+    """Return the version of PARASECT.
+    
+    :return: The version of PARASECT.
+    :rtype: str
+    """
+    return VERSION
