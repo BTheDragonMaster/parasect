754647c4e74bf9ef2b1adf0a3611f1664a9b9297
David Meijer <david-meijer@live.nl>
2024-10-15 16:49:31 +0200

    STY: complied with flake8

--- app/src/server/routes/constants.py
+++ app/src/server/routes/constants.py
@@ -4,6 +4,9 @@
 
 import os
 
-MODEL_DIR_LOCAL = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))), "models")
+MODEL_DIR_LOCAL = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))),
+    "models",
+)
 MODEL_DIR = os.getenv("MODEL_DIR", MODEL_DIR_LOCAL)
 TEMP_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "temp")
