bb4c63d3ed213967c7089451f6db793de3da1e74
David Meijer <david-meijer@live.nl>
2024-10-16 11:21:10 +0200

    ENH: adding scheduler that clears job results every week

--- app/src/server/routes/app.py
+++ app/src/server/routes/app.py
@@ -3,6 +3,33 @@
 """Module for defining the Flask app."""
 
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+import atexit
+
 
 app = Flask(__name__)
 app.config["JOB_RESULTS"] = dict()
+
+
+def clear_job_results() -> None:
+    """Clear the job results."""
+    app.config["JOB_RESULTS"].clear()
+
+
+# set up a scheduler to run the clear_job_results function every week
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=clear_job_results,
+    trigger=IntervalTrigger(weeks=1),
+    id="clear_job_results_job",
+    name="clear job results every week",
+    replace_existing=True,
+)
+
+
+# shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+
