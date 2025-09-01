f1755e76db4605498650392fa1c43630305d1a49
Terlouw, Barbara <barbara.terlouw@wur.nl>
2025-09-01 09:09:18 +0200

    Bugfix: prevent double protein-domain links in database

--- src/parasect/database/populate_database.py
+++ src/parasect/database/populate_database.py
@@ -80,6 +80,8 @@ def create_protein_entries(session: Session, protein_path: str) -> tuple[list[Pr
             new_protein: Protein = Protein(sequence=seq)
             protein_entries.append(new_protein)
             for synonym in synonyms:
+                if synonym == "CAG15009.1":
+                    print("It exists")
                 protein_synonyms.append(ProteinSynonym(synonym=synonym, protein=new_protein))
                 synonym_map[synonym] = new_protein
 
@@ -302,12 +304,13 @@ def link_domains_and_proteins(domain_entries: list[AdenylationDomain],
                               protein_entries: list[Protein],
                               session: Session) -> list[ProteinDomainAssociation]:
     print("Mapping domains to proteins...")
-    links = []
+    links: list[ProteinDomainAssociation] = []
 
     old_protein_entries: list[Protein] = list(session.scalars(select(Protein)).all())
     protein_lookup = _build_synonym_lookup(old_protein_entries + protein_entries)
 
     for domain_entry in domain_entries:
+        seen_links: set[tuple[str, str, int]] = set()
         multiple_matches = False
         domain_numbers = set()
         start_coords_multiple: list[int] = []
@@ -318,25 +321,33 @@ def link_domains_and_proteins(domain_entries: list[AdenylationDomain],
         for domain_synonym in domain_entry.synonyms:
             synonym = domain_synonym.synonym
             protein_name, domain_number = _parse_domain_synonym(synonym)
+            if protein_name == 'CAG15009.1':
+                print("I can also find it here", domain_entry.get_name())
 
             domain_numbers.add(domain_number)
 
             if protein_name in protein_lookup:
+
                 match_found = True
                 protein = protein_lookup[protein_name]
+
 
                 start_coords = [match.start() + 1 for match in re.finditer(domain_entry.sequence, protein.sequence)]
 
                 if len(start_coords) == 0:
                     raise ValueError(f"Domain {synonym} not found in protein {protein_name}")
                 elif len(start_coords) == 1:
+                    start_coord = start_coords[0]
+                    end_coord = start_coord + len(domain_entry.sequence)
+                    link_id = (protein.get_name(), domain_entry.get_name(), start_coord)
 
-                    end_coord = start_coords[0] + len(domain_entry.sequence)
-                    links.append(ProteinDomainAssociation(protein=protein,
-                                                          domain=domain_entry,
-                                                          domain_number=domain_number,
-                                                          start=start_coords[0],
-                                                          end=end_coord))
+                    if link_id not in seen_links:
+                        links.append(ProteinDomainAssociation(protein=protein,
+                                                              domain=domain_entry,
+                                                              domain_number=domain_number,
+                                                              start=start_coord,
+                                                              end=end_coord))
+                        seen_links.add(link_id)
                 else:
                     multiple_matches = True
                     start_coords_multiple = start_coords
@@ -359,12 +370,14 @@ def link_domains_and_proteins(domain_entries: list[AdenylationDomain],
                         start_coord_index = domain_numbers.index(domain_number)
                         start_coord = start_coords_multiple[start_coord_index]
                         end_coord = start_coord + len(domain_entry.sequence)
-
-                        links.append(ProteinDomainAssociation(protein=protein,
-                                                              domain=domain_entry,
-                                                              domain_number=domain_number,
-                                                              start=start_coord,
-                                                              end=end_coord))
+                        link_id = (protein.get_name(), domain_entry.get_name(), start_coord)
+                        if link_id not in seen_links:
+                            links.append(ProteinDomainAssociation(protein=protein,
+                                                                  domain=domain_entry,
+                                                                  domain_number=domain_number,
+                                                                  start=start_coord,
+                                                                  end=end_coord))
+                            seen_links.add(link_id)
 
     return links
 
